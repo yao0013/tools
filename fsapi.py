2cd21cb33c1bca81bb6fe58f1874d6a3c575f3dd
mocanjie@qq.com <mocanjie19850221?>
2023-10-17 17:02:24 +0800

    fastAPI练习

--- fsapi.py
+++ fsapi.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from sqlalchemy import create_engine, text
+from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from pydantic import BaseModel
 
@@ -7,7 +7,7 @@ from pydantic import BaseModel
 app = FastAPI()
 
 # 创建数据库引擎和Session
-DATABASE_URL = "sqlite:///mydatabase.db"
+DATABASE_URL = "mysql://root:gcloud123@localhost/hup"
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
@@ -18,25 +18,23 @@ from sqlalchemy.ext.declarative import declarative_base
 Base = declarative_base()
 
 class Item(Base):
-    __tablename__ = "items"
+    __tablename__ = "title"
 
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String, index=True)
     description = Column(String)
 
-# 创建数据库表
-Base.metadata.create_all(bind=engine)
 
-# 创建Pydantic模型来定义API返回的数据格式
-class ItemResponse(BaseModel):
-    id: int
-    name: str
-    description: str
 
 # 创建FastAPI端点
-@app.get("/items/", response_model=list[ItemResponse])
+@app.get("/items/", response_model=list[Item])
 def get_all_items():
     db = SessionLocal()
     items = db.query(Item).all()
     db.close()
     return items
+
+# 运行FastAPI应用
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
